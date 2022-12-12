839c3c05e83b2768ede79b5b9886b3cd0d795710
ronakdudhani <ronakdudhani@gmail.com>
2022-12-12 21:25:14 +0530

    Task 8 Completed

---
+++ task-08/index.js
@@ -0,0 +1,35 @@
+function wPlay() {
+    var audio = new Audio('sounds/crash.mp3');
+    audio.play();
+}
+
+function sPlay() {
+    var audio = new Audio('sounds/kick-bass.mp3');
+    audio.play();
+}
+
+function aPlay() {
+    var audio = new Audio('sounds/snare.mp3');
+    audio.play();
+}
+
+function dPlay() {
+    var audio = new Audio('sounds/tom-1.mp3');
+    audio.play();
+}
+
+function jPlay() {
+    var audio = new Audio('sounds/tom-2.mp3');
+    audio.play();
+}
+
+function kPlay() {
+    var audio = new Audio('sounds/tom-3.mp3');
+    audio.play();
+}
+
+function lPlay() {
+    var audio = new Audio('sounds/tom-4.mp3');
+    audio.play();
+}
+
